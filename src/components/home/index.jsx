2507dd87f169cc246f0f839576e93c9c3a760cde
Alejandro Linan <jandrolinan@gmail.com>
2020-10-19 14:14:38 -0500

    styled current text and image

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 class Home extends React.Component {
     render(){
         return(
-            <section>
-                <div>
-                    <h1>Ux Research Project</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, harum. Pariatur aperiam id aliquid architecto possimus nesciunt amet ipsum, corporis vitae vel. Esse ducimus in doloremque, officia facilis saepe voluptate?</p>
-                </div>
-                <div>
-                    <img src={require(`./images/minimal-one.jpg`)} placeholder='alt-text' />
+            <section className='home'>
+                <div className='home__head'>
+                    <div className='home__head-text'>
+                        <h1>Ux Research Project</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, harum. Pariatur aperiam id aliquid architecto possimus nesciunt amet ipsum, corporis vitae vel. Esse ducimus in doloremque, officia facilis saepe voluptate?</p>
+                    </div>
+                    <div className='home__head-img'>
+                        <img src={require(`./images/minimal-one.jpg`)} placeholder='alt-text' />
+                    </div>
                 </div>
             </section>
         )
